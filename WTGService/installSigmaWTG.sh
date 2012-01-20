4a68774ccc9ad9b422261cc1498dd740da9ab3ce
Gaurav Jain <gjain@wi-fi.org>
2012-01-20 02:07:15 +0000

    Sigma 4.3.0 11n - Release Testing Modification
    git-svn-id: file:///home/wifi/svnrepos/local-svn/SIG/trunk@46 e6edf6fb-f266-4316-afb4-e53d95876a76

--- WTGService/installSigmaWTG.sh
+++ WTGService/installSigmaWTG.sh
@@ -5,6 +5,21 @@
 sh $PWD/SigmaWTGService.sh stop
 cp  SigmaWTG ../dut/wfa_dut ../ca/wfa_ca /usr/bin
 cp  SigmaWTG.conf /etc
+if [ ! -d /SIGMA_WTG ] 
+then
+	mkdir /SIGMA_WTG
+fi	
+
+if [ ! -d /SIGMA_WTGv2 ]
+then
+        mkdir /SIGMA_WTGv2
+fi
+
+cp  ../scripts/* /usr/bin/
+cp  ../scripts/* /usr/sbin/
+cp  ../scripts/* /usr/local/bin/
+cp  ../scripts/* /usr/local/sbin/
+chmod 777 /usr/local/sbin/*
 rm -f /etc/rc.local
 ln -s $PWD/SigmaWTGService.sh /etc/rc.local
 sh $PWD/SigmaWTGService.sh start
