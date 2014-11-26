97708207314e9c44e9ec41ddc411f49d4bc40969
Dake Zhao <dzhao@wi-fi.org>
2014-11-26 13:59:04 -0800

    Merged the HS2.0-R2 branch into the Sigma-merge branch

--- WTGService/uninstallSigmaWTG.sh
+++ WTGService/uninstallSigmaWTG.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
+#
+# Copyright (c) 2014 Wi-Fi Alliance
+# 
+# Permission to use, copy, modify, and/or distribute this software for any 
+# purpose with or without fee is hereby granted, provided that the above 
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES 
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF 
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY 
+# SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER 
+# RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE
+# USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+
 # Uninstall the SigmaWTG service to system startup
 
 
@@ -6,3 +24,5 @@ sh $PWD/SigmaWTGService.sh stop
 rm -f /usr/bin/SigmaWTG /usr/bin/wfa_dut /usr/bin/wfa_ca
 rm -f /etc/SigmaWTG.conf
 rm -f /etc/rc.local
+rm -f /etc/WfaEndpoint/*
+rmdir  /etc/WfaEndpoint
