5504b4f487a02fb8fe10f9351b5b672becfb666f
Harsh Madhiwalla <hmadhiwalla@wi-fi.org>
2018-01-10 13:42:11 -0800

    -Fix issue with inline declaration

--- lib/wfa_miscs.c
+++ lib/wfa_miscs.c
@@ -155,7 +155,7 @@ int wfa_itime_diff(struct timeval *t1, struct timeval *t2)
  * THe following two functions are converting Little Endian to Big Endian.
  * If your machine is already a Big Endian, you may flag it out.
  */
-inline void int2BuffBigEndian(int val, char *buf)
+void int2BuffBigEndian(int val, char *buf)
 {
     char *littleEn = (char *)&val;
 
